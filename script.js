7c6c81e7f30e0c557d1407941758782699357f6a
Seongmin Lim <tjdalsdk3434@naver.com>
2025-01-15 00:33:37 +0900

    .

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('Welcome to my blog!');
+  console.log('Welcome to SeongMin\'s Blog!');
+
+  const blogContainer = document.getElementById('blog-container');
+  const addPostButton = document.getElementById('add-post');
+
+  addPostButton.addEventListener('click', () => {
+      const post = document.createElement('div');
+      post.className = 'blog-post';
+      post.innerHTML = `
+          <h3>New Blog Post</h3>
+          <p>This is a new blog post. Content will go here.</p>
+      `;
+      blogContainer.appendChild(post);
+  });
 });
